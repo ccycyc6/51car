3f8a9299d9a71d6a2fba8086d336bc45141e1fa9
ccycyc6 <1287315178@qq.com>
2025-06-04 11:39:42 +0800

    Add files via upload

---
+++ 51car/ysyx_car/xunji.c
@@ -0,0 +1,43 @@
+#include <reg52.h>
+#include "motor.h"
+#include "turn.h"
+
+
+sbit left1 = P0^0;
+
+sbit right1 = P0^2;
+
+
+void xunji()
+{
+	unsigned char flag;
+	if((right1 == 0)&&(left1 == 0))
+	{
+		flag = 0;
+	}
+	if((right1 == 0)&&(left1 == 1))
+	{
+		flag = 1;
+	}
+	if((right1 == 1)&&(left1 == 0))
+	{
+		flag = 2;
+	}
+    if((right1 == 1)&&(left1 == 1))
+    {
+        flag = 3;
+    }
+
+	switch(flag)
+	{
+		case 0:qianjin();break;
+		
+		case 1:turn_right1();break;
+		
+		case 2:turn_left1();break;
+
+		case 3:stop();break;
+
+		default:qianjin();break;
+	}
+}
